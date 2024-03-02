c13e566cc848be16058827df17fcec6ef031de5e
Your Name <janengene12@gmail.com>
2024-03-02 12:19:07 +0300

    Small configurations modifications

--- pangaProjo/cypress/e2e/fixtures.cy.ts
+++ pangaProjo/cypress/e2e/fixtures.cy.ts
@@ -1,44 +1,47 @@
-describe('Testing login functionality',()=>{
+describe('Testing login functionality', () => {
 
-    let data:{email:string, password:string};
+    let data: { email: string, password: string };
 
-    before(()=>{
-        cy.fixture('login').then((info)=>{
+    before(() => {
+        cy.fixture('login').then((info) => {
             data = info
         })
     })
 
-    it('logs in user using fixture data', ()=>{
+    it('logs in user using fixture data', () => {
         cy.visit('/login')
 
-        cy.fixture('login.json').then((data)=>{
+        cy.fixture('login.json').then((data) => {
             cy.get('[data-cy="email-link"]').type(data.email)
             cy.get('[data-cy="password-link"]').type(data.password)
-            cy.get('[data-cy="submit-btn"]').click().then(el=>{
-            cy.location('pathname').should('not.equal', '/login')
-            cy.location('pathname').should('equal','/dashboard')
+            cy.get('[data-cy="submit-btn"]').click().then(el => {
+                cy.location('pathname').should('not.equal', '/login')
+                cy.location('pathname').should('equal', '/dashboard')
             })
         })
 
     })
 })
 
-describe('Sending requests without hitting the backend', ()=>{
-    beforeEach(()=>{
+describe('Sending requests without hitting the backend', () => {
+    beforeEach(() => {
         cy.visit('/login')
     })
 
-    it('Post requset handling', ()=>{
+    it('Post requset handling', () => {
         cy.intercept('POST', 'http://localhost:5000/users/login', {
-            body:{
+            body: {
                 message: "Logged in successfully"
             }
-        }).as('RequestToLogin')
+        }).as('RequestToLogin');
 
-        cy.get('[data-cy="submit-btn"]').click()
+        cy.get('[data-cy="submit-btn"]').click();
 
-        cy.wait('@RequestToLogin').then(interception =>{
+        cy.wait('@RequestToLogin', { requestTimeout: 10000 }).then(interception => {
+            console.log('Intercepted request:', interception.request);
+            console.log('Intercepted response:', interception.response);
             expect(interception.request.body).to.exist;
-        })
+        });
+
     })
 })
