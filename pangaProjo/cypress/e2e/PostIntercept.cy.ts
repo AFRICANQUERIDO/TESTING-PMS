0969ce990c8e476f3f0100e648a1c6a9cf522048
Your Name <janengene12@gmail.com>
2024-03-02 14:54:36 +0300

    Successfully fetches data from fixtures

--- pangaProjo/cypress/e2e/PostIntercept.cy.ts
+++ pangaProjo/cypress/e2e/PostIntercept.cy.ts
@@ -3,11 +3,12 @@ describe('Testing sigup functionality using fixtures', () => {
     it('registers a user', () => {
         cy.visit('/register')
         cy.fixture('register.json').then((regDetails) => {
-            cy.get('[data-cy="name-input"]').type(regDetails.name)  
-            cy.get('[data-cy="email-input"]').type(regDetails.email) 
-            cy.get('[data-cy="phone_number-input"]').type(regDetails.phone_number)  
-            cy.get('[data-cy="password-input"]').type(regDetails.password) 
+            cy.get('[data-cy="name-input"]').type(regDetails.name)
+            cy.get('[data-cy="email-input"]').type(regDetails.email)
+            cy.get('[data-cy="phone_number-input"]').type(regDetails.phone_number)
+            cy.get('[data-cy="password-input"]').type(regDetails.password)
             cy.get('[data-cy="create-account-link"]').click().then(el => {
+                cy.wait(1000);
                 cy.visit('/login')
                 cy.location('pathname').should('not.equal', '/register')
                 cy.location('pathname').should('equal', '/login')
@@ -21,21 +22,21 @@ describe('Sending requests to register user without hitting the backend', () => 
     beforeEach(() => {
         cy.visit('/register')
     })
-    cy.intercept('POST', 'http://localhost:5000/users/signup', {
-        body: {
-            message: "User registered successfully"
-        }
-    }).as('RegisterRequest');
 
     it('Post request handling', () => {
+        cy.intercept('POST', 'http://localhost:5000/users/signup', {
+            body: {
+                message: "User registered successfully"
+            },
+            delayMs: 500
+        }).as('RegisterRequest');
 
         cy.get('.form-submit').click();
 
-        cy.wait('@RegisterRequest', { requestTimeout: 10000 }).then(interception => {
+        cy.wait('@RegisterRequest', { requestTimeout: 10000 }).then((interception) => {
             console.log('Intercepted request:', interception.request);
-            console.log('Intercepted response:', interception.response);
             expect(interception.request.body).to.exist;
-        });
+        })
 
     })
 })
@@ -59,6 +60,7 @@ describe('Testing login functionality', () => {
             cy.get('[data-cy="email-link"]').type(data.email)
             cy.get('[data-cy="password-link"]').type(data.password)
             cy.get('[data-cy="submit-btn"]').click().then(el => {
+                cy.wait(1000);
                 cy.visit('/dashboard')
                 cy.location('pathname').should('not.equal', '/login')
                 cy.location('pathname').should('equal', '/dashboard')
@@ -70,7 +72,7 @@ describe('Testing login functionality', () => {
 
 describe('Sending login requests without hitting the backend', () => {
     beforeEach(() => {
-         cy.visit('/login')
+        cy.visit('/login')
         cy.intercept('POST', 'http://localhost:5000/users/login', {
             body: {
                 message: "Logged in successfully"
@@ -78,15 +80,15 @@ describe('Sending login requests without hitting the backend', () => {
         }).as('RequestToLogin');
     })
 
-    it('Sending login requests without hitting the backend', () => {
+    it('Sends login requests without hitting the backend', () => {
 
         cy.get('[data-cy="submit-btn"]').click();
 
-        cy.wait('@RequestToLogin', { requestTimeout: 5000 }).then(interception => {
-            console.log('Intercepted request:', interception.request);
-            console.log('Intercepted response:', interception.response);
-            expect(interception.request.body).to.exist;
-        });
+    cy.wait('@RequestToLogin', { requestTimeout: 5000 }).then(interception => {
+        console.log('Intercepted request:', interception.request);
+        console.log('Intercepted response:', interception.response);
+        expect(interception.request.body).to.exist;
+    });
 
-    })
+})
 })
